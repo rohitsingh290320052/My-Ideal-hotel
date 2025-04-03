eeda04de6dc33048baf956b2737a573d2bb1dd64
upendra <sirfupendra@gmail.com>
2025-04-03 13:35:56 +0530

    authentication done

--- project/backend/app.js
+++ project/backend/app.js
@@ -1,1 +1,22 @@
 const express =require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const db=require('./db/dbconfig');
+const guestRoutes = require('./routes/guests/guest.route');
+db();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+const port=process.env.PORT || 5000;
+app.get('/',(req,res)=>{
+    res.send("Hello World");
+})
+app.use('/guest',guestRoutes);
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
