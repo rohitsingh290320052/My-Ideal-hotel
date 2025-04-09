f0019bb85b4c444a4a87f042c7c09ec832d77750
ABHAYGIRI143 <abhaygiri656@gmail.com>
2025-04-09 15:13:41 +0530

    hii

--- project/backend/app.js
+++ project/backend/app.js
@@ -1,22 +1,34 @@
-const express =require('express');
+const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const db=require('./db/dbconfig');
+const db = require('./db/dbconfig');
+
+// Import Routes
 const guestRoutes = require('./routes/guests/guest.route');
+const staffRoutes = require('./routes/staff/staff.route');
+const hotelRoutes = require('./routes/hotels/hotel.route');
+
+dotenv.config();
 db();
+
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
+const port = process.env.PORT || 5000;
 
-const port=process.env.PORT || 5000;
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("Hello World");
-})
-app.use('/guest',guestRoutes);
+});
 
-app.listen(port,()=>{
+app.use('/guest', guestRoutes);
+app.use('/staff', staffRoutes);
+
+app.use('/hotels', hotelRoutes);
+
+
+
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+});
