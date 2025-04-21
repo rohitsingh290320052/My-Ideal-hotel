08738b7c685779361ef5af80e8b45b95dfd2af02
upendra sisodiya <sirfupendra@gmail.com>
2025-04-21 13:41:04 +0530

    cloudinary configured

--- project/backend/routes/rooms.route.js
+++ project/backend/routes/rooms.route.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const roomController = require('../controllers/rooms.controller');
+const multer = require('multer'); // Import multer for file uploads
+const {storage} = require('../CloudCofig'); // Import the storage configuration for Cloudinary
+const upload = multer({ storage: storage }); // Set the storage configuration for multer
 
-router.post('/create', roomController.addrooms);
+router.post('/create', upload.array('photos', 6),roomController.addrooms);
 
 router.get('/:hotelName', roomController.getrooms); // Get rooms for a specific hotel
 
