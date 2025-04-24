c13709e2d623bb4711e0e23a39365557f30d5c52
upendra sisodiya <sirfupendra@gmail.com>
2025-04-24 13:18:12 +0530

    mergeed with prakhar and abhay

--- routes/guests/guest.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const guestController = require('../../controllers/guests.controller');
-
-router.post('/register', guestController.registerGuest);
-router.post('/login', guestController.loginGuest);
-
-module.exports = router;
